a68f99dc5d289d8a27c1295bf07a7b4a0b11eb26
Stephen Longofono <8992396+SLongofono@users.noreply.github.com>
2018-01-28 17:46:59 -0600

    Working, tested writeback unit

--- RISCV_CORE/writeback.vhd
+++ RISCV_CORE/writeback.vhd
@@ -22,14 +22,14 @@ entity writeback is
     clk:            in std_logic;   -- System clock
     rst:            in std_logic;   -- System reset
     halt:           in std_logic;   -- Do nothing when high
-    read:           in std_logic;   -- MMU requests read
-    write:          in std_logic;   -- Control requests write
-    write_ack:      out std_logic;  -- Write data and address are valid
-    read_ack:       out std_logic;  -- Read data and address recorded
-    read_data:      in doubleword;  -- Data from previous stage
-    read_address:   in word;        -- MMU Destination for read data
-    write_data:     out doubleword; -- Data to be written to MMU
-    write_address:  out word        -- MMU destination for write data
+    ready_input:    in std_logic;   -- Control has data to be written back
+    ready_output:   in std_logic;   -- MMU is ready to accept data
+    output_OK:      out std_logic;  -- Write data and address are valid
+    input_OK:       out std_logic;  -- Read data and address recorded
+    input_data:     in doubleword;  -- Data from previous stage
+    input_address:  in word;        -- MMU Destination for input data
+    output_data:    out doubleword; -- Data to be written to MMU
+    output_address: out word        -- MMU destination for output data
   );
 end writeback;
 
@@ -43,8 +43,8 @@ signal curr_state:  state;
 signal next_state:  state;
 signal last_data:   doubleword;
 signal last_addr:   word;
-signal s_write_ack: std_logic;
-signal s_read_ack: std_logic;
+signal s_output_ack: std_logic;
+signal s_input_ack: std_logic;
 
 begin
 
@@ -54,11 +54,6 @@ begin
     if(rising_edge(clk)) then
         if('1' = rst) then
             curr_state <= state_idle;
-            next_state <= state_idle;
-            last_data <= (others => '0');
-            last_addr <= (others => '0');
-            s_write_ack <= '0';
-            s_read_ack <= '0';
         else
             curr_state <= next_state;
         end if;
@@ -66,34 +61,43 @@ begin
 end process;
 
 -- Compute outputs
-process(curr_state)
+-- Needs to be sensitive to clock in case MMU stalls, we
+-- want to check for updates at each rising edge
+process(clk, rst, curr_state)
 begin
-    next_state <= curr_state;
-    s_write_ack <= '0';
-    s_read_ack <= '0';
-    
-    if('0' = halt) then -- Do nothing unless halt is low
-        case curr_state is
-            when state_writing =>   -- Case pending write
-                if('1' = write) then    -- Case MMU ready for input, signal that write data and addr are ready
-                    s_write_ack <= '1';
-                    next_state <= state_idle;
-                end if;
-                
-            when others =>  -- Case no pending write
-                if('1' = read) then -- Case control has new read data, read and change to write pending state
-                    last_addr <= read_address;
-                    last_data <= read_data;
-                    s_read_ack <= '1';
-                    next_state <= state_writing;
-                end if;
-        end case;
-    end if;
-end process;
+    if(rising_edge(clk)) then
+        input_OK <= '0';
+        output_OK <= '0';
+        output_data <= last_data;
+        output_address <= last_addr;
+        next_state <= curr_state;
 
-write_ack <= s_write_ack;
-read_ack <= s_read_ack;
-write_data <= last_data;
-write_address <= last_addr;
+        if('1' = rst) then    
+            last_data <= (others => '0');
+            last_addr <= (others => '0');
+            output_data <= (others => '0');
+            output_address <= (others => '0');
+            next_state <= state_idle;
+        elsif('0' = halt) then -- Do nothing unless halt is low
+            case curr_state is
+                when state_writing =>   -- Case pending write
+                    output_OK <= '1';  -- signal MMU data is valid
+
+                    if('1' = ready_output) then    -- Case MMU ready for data
+                        next_state <= state_idle;   -- Transition to idle state to get more input
+                    end if;
+                    
+                when state_idle =>  -- Case no pending write
+
+                    if('1' = ready_input) then -- Case control has new data
+                        last_addr <= input_address; -- Update latches
+                        last_data <= input_data;
+                        input_OK <= '1'; -- signal control that data is accepted
+                        next_state <= state_writing;  -- Transition to write pending state
+                    end if;
+            end case;
+        end if; -- rst/halt
+    end if; --  rising edge
+end process;
 
 end Behavioral;
